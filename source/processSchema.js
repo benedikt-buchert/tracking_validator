2ed5508c5b296c4261efa135a8802e2277f7b54b
Benedikt Buchert <benedikt@buchert.digital>
2026-01-28 18:19:21 +0100

    chore: simplyfiy schema

--- source/processSchema.js
+++ source/processSchema.js
@@ -1,11 +1,5 @@
 import $RefParser from "@apidevtools/json-schema-ref-parser";
 
 export async function processSchema(schemaUrl) {
-  try {
-    const bundledSchema = await $RefParser.bundle(schemaUrl);
-    return bundledSchema;
-  } catch (error) {
-    // Re-throw the error to be caught by the caller
-    throw error;
-  }
+  return await $RefParser.bundle(schemaUrl);
 }
